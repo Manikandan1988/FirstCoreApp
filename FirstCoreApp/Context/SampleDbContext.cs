88af027f217bd55a91b4eff2a1d152f341494fd2
Manikandan Sekar <479956@cognizant.com>
2019-12-28 09:26:40 +0530

    initial checked

---
+++ FirstCoreApp/Context/SampleDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FirstCoreApp.Context.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCoreApp.Context
+{
+    public class SampleDbContext : DbContext
+    {
+
+        public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Designation> Designations { get; set; }
+
+    }
+}
